9afe95df53bb394aed516e39644f4ef8d798cd45
ahmeds <ahmeds@ms.com>
2021-12-22 11:28:05 +0200

    Add project files.

---
+++ APIMReporting/Program.cs
@@ -0,0 +1,71 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using CsvHelper;
+using Microsoft.Rest;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace APIMReporting
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello API Management Reporting!");
+            string tenantId = "ENTERTENANTID";
+          
+            var subscriptionId = "SUBSCRIPTIONID";
+            var rgname = "RESOURCEGROUPNAME";
+            var apiServiceName = "APISERVICENAME";
+            var top = 20000;
+            var skip = 0;
+            var count = 20000;
+            var start = "2017-06-01";
+            var end = "2022-06-04";
+            //   might need this for VS
+            //   TokenCredential tokenCredential = new VisualStudioCredential(new VisualStudioCredentialOptions { TenantId = tenantId });
+            TokenCredential tokenCredential = new DefaultAzureCredential();
+
+            //  DefaultAzureCredential
+            TokenRequestContext requestContext = new TokenRequestContext(new string[] { "https://management.azure.com" });
+            CancellationTokenSource cts = new CancellationTokenSource();
+            var accessToken = tokenCredential.GetToken(requestContext, cts.Token);
+            Console.WriteLine("Token: " + accessToken.Token);
+
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Token);
+            using (var writer = new StreamWriter(@"c:\PlayArea\file3.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            { 
+                while (count==20000)
+            {
+
+
+
+                string url = $"https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{rgname}/providers/Microsoft.ApiManagement/service/{apiServiceName}/reports/byRequest?$filter=timestamp ge datetime'{start}' and timestamp le datetime'{end}'&api-version=2021-08-01&$top={top}&$skip={skip}";
+//                    string url = $"https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{rgname}/providers/Microsoft.ApiManagement/service/{apiServiceName}/reports/byRequest?$filter=timestamp ge datetime'2017-06-01' and timestamp le datetime'2022-06-04'&api-version=2021-08-01&$top={top}&$skip={skip}";
+
+                Console.WriteLine(url);
+                Console.WriteLine();
+
+                string json = await client.GetStringAsync(url);
+
+                Rootobject data = JsonSerializer.Deserialize<Rootobject>(json);
+                count = data.count;
+                Console.WriteLine("count:" + count);
+                skip = skip + 20000;
+                csv.WriteRecords(data.value);
+    
+                }
+            }
+
+        }
+    }
+}
---
+++ APIMReporting/Rootobject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIMReporting
+{
+    public class Rootobject
+    {
+        public Value[] value { get; set; }
+        public int count { get; set; }
+    }
+
+    public class Value
+    {
+        public string apiId { get; set; }
+        public string operationId { get; set; }
+        public string productId { get; set; }
+        public string userId { get; set; }
+        public string method { get; set; }
+        public string url { get; set; }
+        public string ipAddress { get; set; }
+        public int? backendResponseCode { get; set; }
+        public int responseCode { get; set; }
+        public int responseSize { get; set; }
+        public DateTime timestamp { get; set; }
+        public string cache { get; set; }
+        public float apiTime { get; set; }
+        public float serviceTime { get; set; }
+        public string apiRegion { get; set; }
+        public string subscriptionId { get; set; }
+        public string requestId { get; set; }
+        public int requestSize { get; set; }
+    }
+
+  
+}
